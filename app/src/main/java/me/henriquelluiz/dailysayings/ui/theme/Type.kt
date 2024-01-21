dd74468c63d70282e180ea2ea58f1d1c014e12b8
Henrique Luiz <heenluy@gmail.com>
2024-01-21 17:19:40 -0300

    set app typography

--- app/src/main/java/me/henriquelluiz/dailysayings/ui/theme/Type.kt
+++ app/src/main/java/me/henriquelluiz/dailysayings/ui/theme/Type.kt
@@ -2,33 +2,37 @@ package me.henriquelluiz.dailysayings.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import me.henriquelluiz.dailysayings.R
 
-// Set of Material typography styles to start with
+val JosefinSans = FontFamily(
+    Font(R.font.josefin_sans_light, FontWeight.Light),
+    Font(R.font.josefin_sans_regular, FontWeight.Normal)
+)
+
+val Melodrama = FontFamily(
+    Font(R.font.melodrama_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = Melodrama,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = JosefinSans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = JosefinSans,
+        fontWeight = FontWeight.Light,
+        fontSize = 11.sp
     )
-    */
 )
